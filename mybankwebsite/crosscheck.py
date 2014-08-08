ea661da486effaf454fd7fcc2a7225213468c306
Scott Umsted <scottumsted@gmail.com>
2014-08-08 13:44:45 -0500

    presentation almost complete

--- mybankwebsite/crosscheck.py
+++ mybankwebsite/crosscheck.py
@@ -1,7 +1,5 @@
 import hashlib
 import uuid
-from flask import request, render_template
-
 
 class CrossCheck:
 
@@ -18,14 +16,3 @@ class CrossCheck:
 
     def _encode_token(self, token):
         return hashlib.sha224(token + self._salt).hexdigest()
-
-
-if __name__ == '__main__':
-    cc1 = CrossCheck()
-    cc2 = CrossCheck()
-    t1, h1 = cc1.generate_token()
-    t2, h2 = cc2.generate_token()
-    print 'one: ' + cc1._salt + ' - ' + t1 + ' - ' + h1
-    print 'two: ' + cc2._salt + ' - ' + t2 + ' - ' + h2
-    print cc1.valid_token(t1, h1)
-    print cc1.valid_token(t2, h1)
